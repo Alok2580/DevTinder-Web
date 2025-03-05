37ae8adadc46efd1d368b19a2987f96cdc9777c6
alok2580 <aloksingh15122004@gmail.com>
2025-03-06 00:17:18 +0530

    done routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,109 +1,34 @@
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+import Footer from "./Footer";
 export default function App() {
   return (
-    // <h1 className="text-3xl font-bold underline bg-red-500">
-    //   Hello world!
-    // </h1>
-
-
-//     <div className="radial-progress" style={{ "--value": 70 }} role="progressbar">
-
-//   70%
-// </div>
+   
 <>
-<div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">🧑‍💻 DevTinder</a>
-  </div>
-  <div className="flex-none">
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-        {/* <div className="indicator"> */}
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"> */}
-            {/* <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-          </svg> */}
-          {/* <span className="badge badge-sm indicator-item">8</span> */}
-        {/* </div> */}
-      </div>
-      <div
-        tabIndex={0}
-        className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
-        {/* <div className="card-body">
-          <span className="text-lg font-bold">8 Items</span>
-          <span className="text-info">Subtotal: $999</span>
-          <div className="card-actions">
-            {/* <button className="btn btn-primary btn-block">View cart</button> */}
-          {/* </div> */}
-        {/* </div>  */}
-      </div>
-    </div>
-    <div className="dropdown dropdown-end mx-10">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
+<BrowserRouter basename="/">
+<Routes>
+  
+<Route path="/" element = {<Body/>}> 
+
+ <Route path = "/login" element={<Login />} />
+<Route path ="/profile" element = {<Profile/>}/>
+
+
+</Route>
 
 {/* 
-<div className="avatar">
-  <div className="w-24 rounded-xl">
-    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-  </div>
-</div>
-<div className="avatar">
-  <div className="w-24 rounded-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-  </div>
-</div> */}
+<Route path="/login" element={<div> Login Page </div>}></Route>
+<Route path="/signUp" element= {<div>signUp</div>}></Route> */}
 
+</Routes>
 
-
-
-
-<div className="card bg-base-100 w-96 shadow-xl">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+</BrowserRouter>
 
 
 
 </>
-
   )
-
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import NavBar from './NavBar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+     
+        {/* <NavBar/> */}
+        <App />
+
+
   </StrictMode>,
 )
