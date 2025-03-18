417d1c85e00132b95e5b84b281e679c56436cef2
alok2580 <aloksingh15122004@gmail.com>
2025-03-19 00:16:13 +0530

    updated

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import BASE_URL from '../utils/constants';
+// import { useNavigate } from 'react-router-dom';
+import { addFeed } from '../utils/feedSlice';
+import UserCard from './UserCard';
+
+
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
+
+const feed= useSelector((store)=>store.feed);
+const dispatch = useDispatch();
+// const navigate = useNavigate();
+
+const getFeed = async()=>{
+
+// if(feed && feed.length) return ;
+
+try{
+
+const res= await axios.get(BASE_URL+"/feed",{withCredentials:true});
+
+// console.log("res",res);
+
+
+dispatch(addFeed(res?.data));
+
+}
+
+catch(err){
+console.log("ERROR"+ err.message);
+}
+
+}
+
+useEffect(()=>{
+  getFeed();
+},[]);
+
+// console.log("feed",feed);
+
+// userCard(user);
+
+  return feed && (
+
+    <div className='flex justify-center '>
+    
+     < UserCard user={feed[feed.length-5]} />
+
+    </div>
+
   )
+
+
 }
 
 export default Feed;
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const UserCard = (user) => {
+
+// console.log("user",user);
+  const {firstName, lastName, photoId,bio,gender,age}= user.user;
+  // console.log(firstName);
+  return (
+    <>
+      <div className="card bg-base-300 w-96 shadow-sm pd-5">
+        <figure>
+          <img
+            src={photoId}
+            alt="Shoes"
+            style={{ maxWidth: '300px', maxHeight: '300px' }}
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            {firstName} {lastName}
+          </h2>
+          { age && gender && (<p>{age} {gender}</p>)}
+          <p>{bio}</p>
+          <div className="card-actions justify-end">
+          <button className="btn btn-primary">ignore</button>
+            <button className="btn btn-secondary">Interested</button>
+           
+          </div>
+        </div>
+      </div>
+    </>
+  )
+
+}
+
+
+
+export default UserCard
